cf3e474780f005ffb83b711714fffa0ecad00982
PeterZarate <pedroazarateflores@icon.edu.mx>
2025-03-09 16:40:20 -0600

    Agregando scrpit.js para saludo dinamico
    Archivo JS para saludo dinamico dependiendo de la hora en la que se ingrese

---
+++ script.js
@@ -0,0 +1,10 @@
+const saludo = document.getElementById("saludo");
+const hora = new Date().getHours();
+
+if (hora < 12){
+  saludo.textContent = "!Buenos días¡";
+} else if (hora < 18) {
+  saludo.textContent = "!Buenas tardes¡";
+} else {
+  saludo.textContent = "!Buenas noches¡";
+}
